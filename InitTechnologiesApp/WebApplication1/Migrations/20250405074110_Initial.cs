69dd8e7b8cf16edd5afd937617a6942c292c85fd
djuka10 <vikac.parkic@gmail.com>
2025-04-05 09:45:24 +0200

    Add crud operation on backend side

---
+++ InitTechnologiesApp/WebApplication1/Migrations/20250405074110_Initial.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Vehicles",
+                columns: table => new
+                {
+                    VehicleId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Year = table.Column<int>(type: "int", nullable: false),
+                    FuelType = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehicles", x => x.VehicleId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Vehicles",
+                columns: new[] { "VehicleId", "FuelType", "Name", "Year" },
+                values: new object[,]
+                {
+                    { 1, "Petrol", "Skoda Kamiq", 2022 },
+                    { 2, "Petrol", "Skoda Superb", 2022 },
+                    { 3, "Diesel", "Opel Vectra", 2002 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vehicles_Name",
+                table: "Vehicles",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Vehicles");
+        }
+    }
+}
---
+++ InitTechnologiesApp/WebApplication1/Program.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using WebApplication1.Database;
+using WebApplication1.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("EnableCORS", builder =>
+    {
+        builder.AllowAnyOrigin()
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
+
+
+builder.Services.AddDbContext<ApplicationDbContext>();
+
+builder.Services.AddScoped<IVehicleRepository, VehicleRepository>();
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseStaticFiles();
+
+app.UseCors("EnableCORS");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
